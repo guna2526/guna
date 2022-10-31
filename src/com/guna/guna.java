96942c849478cd296bcaaf5d3dc8f12b211c0e1c
guna2526 <gunanavaraj@gmail.com>
2022-10-31 20:57:09 +0530

    bablu task

--- src/com/guna/guna.java
+++ src/com/guna/guna.java
@@ -1,5 +1,15 @@
 package com.guna;
 
 public class guna {
-
+private void empName() {
+System.out.println("EmpName: Guna");
+}
+private void empId() {
+System.out.println("EmpId: 1231865");
+}
+public static void main(String[] args) {
+	guna g = new guna();
+	g.empName();
+	g.empId();
+}
 }
